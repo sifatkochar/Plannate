9a36f1a587eb63ac822f7842aefad74e0db736d5
sifatkochar <sifat25493@iiitd.ac.in>
2026-06-01 14:16:02 +0530

    added emoji()

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ import matplotlib.pyplot as plt
 from oclock import Countdown
 from datetime import date
 from matplotlib import image as mpimg
+
+print("******************************")
+print("----PLANNATE WELCOMES YOU!----")
+print("******************************")
+print("Do you wish to LOGIN / SIGNUP?")
+ls = input()
+
+def emoji():
+    global img
+    global titl
+    print(img,img,img,img,img,img,img,img,img)
+    print(titl)
+    print(img,img,img,img,img,img,img,img,img)
+
