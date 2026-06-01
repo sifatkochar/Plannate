12bcf4c3c5ee3b475f901e96421e272160499afd
sifatkochar <sifat25493@iiitd.ac.in>
2026-06-01 20:53:26 +0530

    added generate_speed_graph()

--- main.py
+++ main.py
@@ -117,3 +117,95 @@ def generate_time_graph():
 
     menu()
 
+def generate_speed_graph():
+    global img
+    global titl
+    cursor = mydb.cursor()
+
+    chem_speed_query = '''SELECT monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM JEE_SPEED WHERE subject = "chemistry"'''
+    cursor.execute(chem_speed_query)
+    chemistry_speed = cursor.fetchall()
+
+    physics_speed_query = '''SELECT monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM JEE_SPEED WHERE subject = "physics"'''
+    cursor.execute(physics_speed_query)
+    physics_speed = cursor.fetchall()
+
+    maths_speed_query = '''SELECT monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM JEE_SPEED WHERE subject = "maths"'''
+    cursor.execute(maths_speed_query)
+    maths_speed = cursor.fetchall()
+
+    mixed_speed_query = '''SELECT monday, tuesday, wednesday, thursday, friday, saturday, sunday FROM JEE_SPEED WHERE subject = "mixed"'''
+    cursor.execute(mixed_speed_query)
+    mixed_speed = cursor.fetchall()
+
+    sdays = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+    chem_speed_values = []
+    physics_speed_values = []
+    maths_speed_values = []
+    mixed_speed_values = []
+
+    for row in chemistry_speed:
+        for i in row:
+            chem_speed_values.append(i)
+    for row in physics_speed:
+        for i in row:
+            physics_speed_values.append(i)
+    for row in maths_speed:
+        for i in row:
+            maths_speed_values.append(i)
+    for row in mixed_speed:
+        for i in row:
+            mixed_speed_values.append(i)
+
+    total_speed_values = chem_speed_values + physics_speed_values + maths_speed_values + mixed_speed_values
+    total_valid_speed_values = []
+    for value in total_speed_values:
+        if value not in (0, None):
+            total_valid_speed_values.append(value)
+    
+    try:
+        avg_speed = sum(total_valid_speed_values) / len(total_valid_speed_values)
+        
+        if avg_speed < 1.5:
+            titl = "AMAZING WORK! KEEP GOING."
+            img = "\U0001F603"
+            emoji()
+        elif avg_speed > 3.5:
+            titl = "You need to work on your PACE!"
+            img = "\U0001F61E"
+            emoji()
+        else:
+            titl = "PERFECT!"
+            img = "\U0001F60A"
+            emoji()
+    
+    except ZeroDivisionError:
+        print('Please enter some RECORDS.')
+
+    
+    mask_mixed = np.array([True if value == 0 or value is None else False for value in mixed_speed_values])
+    mixed_speed_values = np.ma.masked_array(mixed_speed_values, mask = mask_mixed)
+
+    mask_maths = np.array([True if value == 0 or value is None else False for value in maths_speed_values])
+    maths_speed_values = np.ma.masked_array(maths_speed_values, mask = mask_maths)
+
+    mask_physics = np.array([True if value == 0 or value is None else False for value in physics_speed_values])
+    physics_speed_values = np.ma.masked_array(physics_speed_values, mask = mask_physics)
+
+    mask_chem = np.array([True if value == 0 or value is None else False for value in chem_speed_values])
+    chem_speed_values = np.ma.masked_array(chem_speed_values, mask = mask_chem)
+    
+  
+    x_axis = np.arange(1, 8, 1)
+    plt.plot(x_axis, chem_speed_values, 'o-', color = 'blue', label = 'CHEMISTRY')
+    plt.plot(x_axis, physics_speed_values, 'o-', color = 'red', label = 'PHYSICS')
+    plt.plot(x_axis, maths_speed_values, 'o-', color = 'green', label = 'MATHS')
+    plt.plot(x_axis, mixed_speed_values, 'o-', color = 'yellow', label = 'MIXED')
+
+    plt.ylabel('QUESTIONS / 10-MINUTES')
+    plt.title('SPEED GRAPH')
+    plt.xticks(x_axis, sdays)
+    plt.legend()
+    plt.show()
+
+    menu()
